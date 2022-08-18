4202dd112bcdbad5d7a90be6e6e6eb5e27429109
Felipe Cruz <felipe@rxredefined.com>
2022-08-18 11:28:23 -0700

    feat: add material-ui v5

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,28 @@
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import Head from "next/head";
+import * as React from "react";
+
+import emotionCached from "../config/emotionCache";
+import theme from "../config/theme";
 import Layout from "../components/Layout";
 
-const App = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-);
+const clientSideEmotionCache = emotionCached();
 
-export default App;
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
